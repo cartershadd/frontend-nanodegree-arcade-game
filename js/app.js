28d8ad3558ea484c520d67da41fc0eb83f678473
Carter-Shadden <carter.shadden@gmail.com>
2018-08-11 18:03:15 -0400

    Made player respond to keypad commands, randomized enemy speeds, made player reset at water and at enemy collisions.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,11 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = -100 - (Math.random() * 500);
+    this.y = 50 + (Math.floor(Math.random() * 3) * 85);
+    this.speed = (Math.random() * 5);
+    this.height = 50;
+    this.width = 50;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +19,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed;
+    if (this.x >= 505) {
+      this.x = -100;
+      this.y = 50 + (Math.floor(Math.random() * 3) * 85);
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +34,64 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+
+
+  this.sprite = 'images/char-cat-girl.png';
+  this.height = 50;
+  this.width = 50;
+  this.x = 200;
+  this.y = 400;
+};
+
+Player.prototype.update = function(dt) {
+  allEnemies.forEach(function(enemy) {
+    if (player.x + player.width > enemy.x &&
+      player.y + player.height > enemy.y &&
+      enemy.x + enemy.width > player.x &&
+      enemy.y + enemy.height > player.y) {
+        player.x = 200;
+        player.y = 400;
+    }
+  });
+  if (this.y < 10) {
+    this.x = 200;
+    this.y = 400;
+  }
+};
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(direction) {
+    this.direction = direction;
+    if (this.direction === 'left' && this.x > 0) {
+      this.x -= 100;
+    } else if (this.direction === 'right' && this.x < 400) {
+      this.x += 100;
+    } else if (this.direction === 'up') {
+      this.y -= 85;
+    } else if (this.direction === 'down' && this.y < 400) {
+      this.y += 85;
+    }
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+const allEnemies = [];
+// //allEnemies.push(new Enemy());
+// allEnemies.push(new Enemy());
+// allEnemies.push(new Enemy());
+// allEnemies.push(new Enemy());
+// allEnemies.push(new Enemy());
+// allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
 
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
